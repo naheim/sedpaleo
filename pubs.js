bb30a9d96921d9184866a54b79b8d34dcf52198d
naheim <naheim@gmail.com>
2020-04-23 14:55:57 -0400

    more changes

--- pubs.js
+++ pubs.js
@@ -55,15 +55,15 @@ $.get('peerreviewed.tsv', function(data) {
 					vol = " " + columns[5] + ":" + columns[7];					
 				}
 				html += "<td><strong>" + columns[3] + ".</strong><em> " + columns[4] + "</em>" + vol; // title and Journal/vol/issue/pages
-				html += ".<br/><small>" + columns[1] + ". " + columns[2] + ".</small>"; // author and year
+				html += ".<br/><small>" + columns[1] + ". " + columns[2] + "."; // author and year
 					
 			} else {
 				html += "<td><strong>" + columns[3] + ".</strong> <em>In</em> " + columns[10] + ". " + columns[9] + ". " + columns[7]; // title and editors and book title
-				html += ".<br/><small>" + columns[1] + ". " + columns[2] + ".</small>"; // author and year
+				html += ".<br/><small>" + columns[1] + ". " + columns[2] + "."; // author and year
 			}
 			
 			// add doi and altmetric
-			html += doialtmet;
+			html += doialtmet + ".</small>";
 			
 			// add note
 			if(columns[15] != "") {
