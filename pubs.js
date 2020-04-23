ddc1dfb58e39cff1845758b6705bb961f8202445
naheim <naheim@gmail.com>
2020-04-23 12:35:00 -0400

    formatting journal articles

--- pubs.js
+++ pubs.js
@@ -4,7 +4,7 @@ $.get('peerreviewed.tsv', function(data) {
 	var html = '<h2>Peer-Reviewed Publications</h2><table class="w3-table w3-striped w3-bordered w3-hoverable">';
 	
 	//
-	var lineCount = data.split('\n').length - 1;
+	var lineCount = data.split('\n').length;
 	//console.log(lineCount);
 	
 	// split into lines
@@ -18,17 +18,30 @@ $.get('peerreviewed.tsv', function(data) {
 		
 		if(columns[0] == "type") {
 			var colNames = columns;
-			console.log(columns[jQuery.inArray( "title", columns )]);
+			//console.log(columns[jQuery.inArray( "title", colNames )]);
 			
 		} else {
 			// start a table row
 			html += "<tr>";
-
-		
-
-			html += "<td>" + columns[0] + "</td>";
-			html += "<td>" + columns[1] + "</td>";
-			html += "<td>" + columns[2] + "</td>";
+			
+			// cell with paper count
+			html += "<td class='rownumber'>" + lineCount - i + "</td>";
+			
+			// format citation cell if journal 
+			if(columns[jQuery.inArray( "type", colNames )] == "journal") {
+				html += "<td><strong>" + columns[jQuery.inArray( "title", colNames )] + ".</strong><em> " + columns[jQuery.inArray( "journal", colNames )] + "</em>"; // title and Journal
+				if(columns[jQuery.inArray( "notyetpub", colNames )] != "") {
+					html += columns[jQuery.inArray( "notyetpub", colNames )]; // in press etc. 	
+				}
+				html += ".<br/><small>" + columns[jQuery.inArray( "authors", colNames )] += ". " + columns[jQuery.inArray( "year", colNames )] + "."; // author and year
+				if(columns[jQuery.inArray( "doi", colNames )] != "") {
+					html += " <a href='http://doi.org/" + columns[jQuery.inArray( "doi", colNames )] + "' target='blank'>doi:" + columns[jQuery.inArray( "doi", colNames )] += "</a>."; // doi
+					html += "<div class='altmetric-embed' data-badge-type='1' data-badge-popover='right' data-doi='" + columns[jQuery.inArray( "doi", colNames )] + "' data-link-target='_blank' style='display: inline-block; vertical-align: middle;'></div>"
+				}	
+			}
+			
+			// close cell	
+			html += "</td>";
 
 			// close row
 			html += "</tr>";
